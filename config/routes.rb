001777efcf45ac94f4433244df00d80dbcfef538
Simon <simonireilly@gmail.com>
2017-01-21 21:51:48 +0000

    Adding nested routes - we'll see if it works

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root to: 'home#index'
+  root to: 'users#home'
+
+  resources :users do
+    resource :data
+  end
 end

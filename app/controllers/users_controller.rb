4b072ad7bf3cd3f0c0dee82f3517a0ef59765cde
Simon <simonireilly@gmail.com>
2017-01-22 20:25:29 +0000

    Adding the leg day indicator

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -34,7 +34,7 @@ class UsersController < ApplicationController
     if current_user && current_user.data.count >= 1
 
       (current_user.data.last.created_at.to_date..current_user.data.last.created_at.to_date).each do |date|
-        Datum.where(created_at: date.midnight..date.end_of_day).all.each do |datum|
+        current_user.data.where(created_at: date.midnight..date.end_of_day).all.each do |datum|
           @returnedData += datum.weight*datum.reps*datum.sets
         end
         @intensityArray.push([date, @returnedData])

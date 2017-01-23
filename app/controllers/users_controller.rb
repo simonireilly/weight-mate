33ce74bfa6747677172cc323d53e4829b0fc5ee6
Simon <simonireilly@gmail.com>
2017-01-23 07:35:02 +0000

    Fixed first/last discrepancy in users controller intensity

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -33,7 +33,8 @@ class UsersController < ApplicationController
 
     if current_user && current_user.data.count >= 1
 
-      (current_user.data.last.created_at.to_date..current_user.data.last.created_at.to_date).each do |date|
+      (current_user.data.first.created_at.to_date..current_user.data.last.created_at.to_date).each do |date|
+        @returnedData = 0
         current_user.data.where(created_at: date.midnight..date.end_of_day).all.each do |datum|
           @returnedData += datum.weight*datum.reps*datum.sets
         end
